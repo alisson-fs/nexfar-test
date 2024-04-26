c42aca04f11d297bf3e14471e7b1fd8ef0a705fc
AlissonFS <alissonfs100@gmail.com>
2024-04-26 19:25:04 -0300

    feat: criação do projeto

---
+++ src/main/java/br/alisson/nexfartest/NexfarTestApplication.java
@@ -0,0 +1,13 @@
+package br.alisson.nexfartest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexfarTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexfarTestApplication.class, args);
+	}
+
+}
